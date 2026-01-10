c36caea46c4203299f5c8430b65de5c0a61568dc
Veloc1tyE <w.jeremijenko@gmail.com>
2026-01-10 17:34:19 +1000

    Add .vercel/ to gitignore (build artifacts should not be tracked)

--- .vercel/output/_functions/chunks/content-modules_B4auQ0aw.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/essays/create-an-age-of-wonders.mdx", () => import('./create-an-age-of-wonders_CQSfhehd.mjs')]]);
-
-export { contentModules as default };
