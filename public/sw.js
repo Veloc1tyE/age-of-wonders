c13a08320e4cfd421cc87377023a9a8f0d5d38fc
Veloc1tyE <w.jeremijenko@gmail.com>
2026-06-14 16:19:17 +1000

    bump cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Age of Wonders
 // Stale-while-revalidate: instant from cache, fresh in background
 
-const CACHE_VERSION = 'age-of-wonders-v94';
+const CACHE_VERSION = 'age-of-wonders-v95';
 
 // Install event - take over immediately
 self.addEventListener('install', () => {
