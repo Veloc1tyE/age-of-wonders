02c0dec5cc9bc73377ec7b70e6c5fa5b26d34808
Veloc1tyE <w.jeremijenko@gmail.com>
2026-07-06 22:03:04 +1000

    clean up

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Age of Wonders
 // Stale-while-revalidate: instant from cache, fresh in background
 
-const CACHE_VERSION = 'age-of-wonders-v154';
+const CACHE_VERSION = 'age-of-wonders-v155';
 
 // Install event - take over immediately
 self.addEventListener('install', () => {
